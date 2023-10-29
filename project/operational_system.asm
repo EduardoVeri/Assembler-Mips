59d655dede9cd267f53a42287dcb37a1938a13a9
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-10-29 16:16:50 -0300

    Update comments

--- project/operational_system.asm
+++ project/operational_system.asm
@@ -23,7 +23,7 @@ main:
     disp $zero $zero 1             # Imprimir no display as opções disponíveis para o usuário
     in $t0 $zero 0              
 
-    beq $t0 $t1 escolha1           # Se o usuário digitar 1, executar um programa em Assembly MIPS reduzida
+    beq $t0 $t1 escolha1           # Se o usuário digitar 1, executar um programa
     beq $t0 $t2 escolha2           # Se o usuário digitar 2, executar vários programas com preempção
     j main                         # Volta para o início do programa
 
@@ -31,14 +31,14 @@ main:
 escolha1:                       
     disp $zero $zero 2             # Imprimir no display os programas disponíveis para o usuário
     in $t0 $zero 0              
-    # Verifica se o valor está entre 1 e 10 (sem usar pseudo-instruções)
 
+    # Verifica se o valor está entre 1 e 10
     ori $t4 $zero 1               
-    slti $t1 $t0 1                # Se o valor for menor que 0, $t1 = 1
-    slti $t2 $t0 10             # Se o valor for menor que 10, $t2 = 1
-    xori $t2 $t2 1                # Inverte o valor de $t2
-    or $t3 $t1 $t2                # Se $t1 = 1 ou $t2 = 1, $t3 = 1
-    beq $t3 $t4 escolha1          # Se $t3 = 0, o valor está entre 1 e 10
+    slti $t1 $t0 1                 # Se o valor for menor que 0, $t1 = 1
+    slti $t2 $t0 10                # Se o valor for menor que 10, $t2 = 1
+    xori $t2 $t2 1                 # Inverte o valor de $t2
+    or $t3 $t1 $t2                 # Se $t1 = 1 ou $t2 = 1, $t3 = 1
+    beq $t3 $t4 escolha1           # Se $t3 = 0, o valor está entre 1 e 10
     
     pc $t2 $zero 0               
     addi $t2 $t2 7              
