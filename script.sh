f6adf7c2b27c98754e047a18bddc91ae9f60e138
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-10-29 01:06:07 -0300

    Changed .txt to .asm

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-python ./src/assembler.py ./project/operational_system.txt ./bin/bin.txt
+python ./src/assembler.py ./project/operational_system.asm ./bin/bin.txt
 python ./src/binary_concatenator.py 
