ceaf42cf4092e91b9dd28d10215dc2b5e197b966
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-11-21 21:17:53 -0300

    Task 2 finalized

--- project/operational_system.asm
+++ project/operational_system.asm
@@ -126,11 +126,11 @@ escolha2:
         beq $t3 $zero else2
             # TODO: Aqui eu preciso pegar da memória o valor do pc do programa que está sendo executado salvo na memória
             add $t6 $zero $t2 # $t6 = id
-            subi $t6 $t6 1
+            # subi $t6 $t6 1
             ori $t4 $zero 33 
             mul $s0 $t6 $t4
 
-            lw $s2 contexto($t4) # 33*ID + 2035
+            lw $s2 contexto($s0) # 33*ID + 2035
             
             jal carrega_contexto # Carrega o contexto do programa que está sendo executado
             
@@ -161,18 +161,83 @@ escolha2:
 
         nop 
         
-        checkint $
+        # --- Aqui tudo precisa ser feito com registradores reservados ---
+        checkint $s0 $zero 0 # Verifica qual interrupção ocorreu
+
         ori $s1 $zero 1
         slt $s2 $s1 $s0
         slt $s2 $s0 $s1
         xori $s2 $s2 1
 
+        # --- Interrupção de tempo - Levar para o final da fila de execução ---
         beq $s2 $zero else3
-            j salva_contexto
-            
-            j end_if3
-        else3:
+            lw $s2 id_procs($zero) # $s0 = id do programa que está sendo executado
+            # subi $t6 $t6 1
+            ori $s1 $zero 33 
+            mul $s0 $s2 $s1
 
+            pci $s1 $zero 0 # Salva o pc do programa que está sendo executado
+            sw $s1 contexto($s0) # Salva o contexto do programa que está sendo executado
+            
+            addi $s0 $s0 28
+            sw $ra contexto($s0) # Salva o $ra do programa que estava sendo executado
+            subi $s0 $s0 28
+            
+            jal salva_contexto
+            
+            ori $t0 $zero 0
+            add $t1 $zero $s2  # buffer_proc = id_procs[0]
+            lw $t6 state_procs($zero) # buffer_state = state_procs[0]
+            lw $t2 var_total($zero) 
+            subi $t2 $t2 1
+                
+            while3:
+                slt $t3 $t0 $t2
+                beq $t3 $zero end_while3
+                
+                addi $t4 $t0 id_procs
+                lw $t5 1($t4) # $t5 = id_procs[i+1]
+                sw $t5 id_procs($t0) # id_procs[i] = id_procs[i+1]
+                
+                addi $t4 $t0 state_procs
+                lw $t5 1($t4) # $t5 = state_procs[i+1]
+                sw $t5 state_procs($t0) # state_procs[i] = state_procs[i+1]
+
+                addi $t0 $t0 1
+
+                j while3
+
+            end_while3:
+
+            sw $t1 id_procs($t2) # id_procs[total-1] = buffer_proc
+            sw $t6 state_procs($t2) # state_procs[total-1] = buffer_state
+
+            j end_if3
+
+        # --- Halt do programa. Excluir o programa da lista de programas a serem executados ---
+        else3:
+            ori $t0 $zero 0 # i = 0
+            lw $t1 var_total($zero) # total = var_total
+            subi $t1 $t1 1
+
+            while4:
+                slt $t2 $t0 $t1
+                beq $t2 $zero end_while4
+
+                addi $t3 $t0 id_procs
+                lw $t4 1($t3) # $t4 = id_procs[i+1]
+                sw $t4 id_procs($t0) # id_procs[i] = id_procs[i+1]
+                
+                addi $t3 $t0 state_procs
+                lw $t4 1($t3) # $t4 = state_procs[i+1]
+                sw $t4 state_procs($t0) # state_procs[i] = state_procs[i+1]
+
+                addi $t0 $t0 1
+
+                j while4
+            end_while4:
+
+            sw $t1 var_total($zero) # var_total = total - 1
 
         end_if3:
         
@@ -242,4 +307,64 @@ carrega_contexto:
     # Não esquecer de salvar o $ra na main
     jr $zero $ra $zero
     
+salva_contexto:
+    addi $s0 $s0 1
+    sw $t0 contexto($s0)
+    addi $s0 $s0 1
+    sw $t1 contexto($s0)
+    addi $s0 $s0 1
+    sw $t2 contexto($s0)
+    addi $s0 $s0 1
+    sw $t3 contexto($s0)
+    addi $s0 $s0 1
+    sw $t4 contexto($s0)
+    addi $s0 $s0 1
+    sw $t5 contexto($s0)
+    addi $s0 $s0 1
+    sw $t6 contexto($s0)
+    addi $s0 $s0 1
+    sw $t7 contexto($s0)
+    addi $s0 $s0 1
+    sw $t8 contexto($s0)
+    addi $s0 $s0 1
+    sw $t9 contexto($s0)
+    addi $s0 $s0 1
+    sw $t10 contexto($s0)
+    addi $s0 $s0 1
+    sw $t11 contexto($s0)
+    addi $s0 $s0 1
+    sw $t12 contexto($s0)
+    addi $s0 $s0 1
+    sw $t13 contexto($s0)
+    addi $s0 $s0 1
+    sw $t14 contexto($s0)
+    addi $s0 $s0 1
+    sw $t15 contexto($s0)
+    addi $s0 $s0 1
+    sw $t16 contexto($s0)
+    addi $s0 $s0 1
+    sw $t17 contexto($s0)
+    addi $s0 $s0 1
+    sw $t18 contexto($s0)
+    addi $s0 $s0 1
+    sw $t19 contexto($s0)
+    addi $s0 $s0 1
+    sw $t20 contexto($s0)
+    addi $s0 $s0 1
+    sw $t21 contexto($s0)
+    addi $s0 $s0 1
+    sw $t22 contexto($s0)
+    addi $s0 $s0 1
+    sw $pilha contexto($s0)
+    addi $s0 $s0 1
+    sw $temp contexto($s0)
+    addi $s0 $s0 1
+    sw $sp contexto($s0)
+    addi $s0 $s0 1
+    sw $fp contexto($s0)
+
+    # Não esquecer de salvar o $ra na main
+    jr $zero $ra $zero
+
+
 
