efec99789741b60e061cfeaf65d320fbf2c53459
eduardoveri <verissimo.eduardo@unifesp.br>
2025-03-10 20:59:30 -0300

    Rename folder to OS_project

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-python ./src/assembler.py ./project/operational_system.asm ./bin/bin.txt
+python ./src/assembler.py ./OS_project/operational_system.asm ./bin/bin.txt
 python ./src/binary_concatenator.py 
